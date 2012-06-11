4dac31b00e57cf436408dd37229979f7500eb181
Steven Rojas <stevenrojascr@miniLeo.local>
2012-06-10 20:14:37 -0600

    Inicio. rails new steico -d=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Steico::Application.config.secret_token = '1a0b8013c09d29ca31c9e1ea710609a34f94287626f12ab2eefe41a8b328055b2933be8d2acad74072b3923bd712f44e916f4fe7a7482fa2ca209745a8cf299b'
