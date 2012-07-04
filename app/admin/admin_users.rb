ae15fe621773a90e57623bf62890f4324f651980
Steven Rojas <stevenrojascr@gmail.com>
2012-07-03 18:27:24 -0600

    Se agregó proyectos

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -6,4 +6,10 @@ ActiveAdmin.register AdminUser do
     column :sign_in_count
     default_actions
   end
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :email
+    end
+    f.buttons
+  end
 end
