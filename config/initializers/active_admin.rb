6b4ab875cf5d996e2eabfe856e2f9561d87ed1a2
Steven Rojas <stevenrojascr@aca80167.ipt.aol.com>
2012-07-21 12:29:26 -0600

    Arreglo temporal para corregir error que impedía crear comentarios

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -127,3 +127,9 @@ ActiveAdmin.setup do |config|
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
 end
+
+module ActiveAdmin
+  class Comment < ActiveRecord::Base
+    attr_accessible :body, :namespace, :resource_id, :resource_type
+  end
+end
